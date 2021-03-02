6aac9b62fa171b59b4392141cb6e8616843352a0
r-m-hufford <ryan@rmhufford.com>
2021-03-02 17:05:42 -0500

    confusing

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -10,24 +10,35 @@ public class PersonHandler {
         this.personArray = personArray;
     }
 
+
     public String whileLoop() {
         String result = "";
-        // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
+        Person currentPerson;
+        int counter = 0;
+        while (counter < personArray.length) {
+            currentPerson = personArray[counter];
+            result = result + currentPerson.toString();
+            counter++;
+        }
+        System.out.println(result);
         return result;
     }
+    //
+        // create a `counter`
+        // while `counter` is less than length of array
+        // begin loop
 
+        // use `counter` to identify the `current Person` in the array
+        // get `string Representation` of `currentPerson`
+        // append `stringRepresentation` to `result` variable
 
+        // end loop
 
     public String forLoop() {
         String result = "";
+        for (int i = 0; i < personArray.length; i++) {
+            result += personArray[i];
+        }
         // identify initial value
         // identify terminal condition
         // identify increment
@@ -46,6 +57,12 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+
+        for (Person person : personArray) {
+            result += person;
+        }
+
+
         // identify array's type
         // identify array's variable-name
 
